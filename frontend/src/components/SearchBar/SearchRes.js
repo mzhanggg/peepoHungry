f8b539181201019f968a61ddb5c3e89d7354a955
Michele Zhang <michelezhang98@gmail.com>
2023-07-13 00:20:29 -0400

    css for searchBar

--- frontend/src/components/SearchBar/SearchRes.js
+++ frontend/src/components/SearchBar/SearchRes.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { fetchSearchBusinesses } from '../../store/businessReducer';
 import BusinessIndexItem from '../Business/BusinessIndexItem';
 
 const SearchRes = () => {
     const { query } = useParams()
     const dispatch = useDispatch();
-
     const searchedBusinesses = useSelector(state => state.businesses)
-    
     const bizItems = Object.values(searchedBusinesses).map(biz => <BusinessIndexItem biz={biz} key={biz.id}/>)
     
     useEffect(() => {
         dispatch(fetchSearchBusinesses(query))
     }, [query])
 
+    console.log(query)
     const emptySearch = () => {
         if ( Object.keys(searchedBusinesses).length === 0 ) {
             return (
                 <div id="empty-search">
-                    <h2>No results for {query}</h2>
+                    <h2>No results for { query }</h2>
                     <ul>Suggestions for improving your results:
                         <li>Check the spelling or try alternate spellings</li>
                         <li>Try a more general search, e.g. "pizza" instead of "pepperoni"</li>
