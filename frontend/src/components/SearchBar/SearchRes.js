89e92477f1331d4519966d1511da957d46fefabc
Michele Zhang <michelezhang98@gmail.com>
2023-07-12 23:48:54 -0400

    add empty search results

--- frontend/src/components/SearchBar/SearchRes.js
+++ frontend/src/components/SearchBar/SearchRes.js
@@ -11,13 +11,29 @@ const SearchRes = () => {
     const searchedBusinesses = useSelector(state => state.businesses)
     
     const bizItems = Object.values(searchedBusinesses).map(biz => <BusinessIndexItem biz={biz} key={biz.id}/>)
-
+    
     useEffect(() => {
         dispatch(fetchSearchBusinesses(query))
     }, [query])
 
+    const emptySearch = () => {
+        if ( Object.keys(searchedBusinesses).length === 0 ) {
+            return (
+                <div id="empty-search">
+                    <h2>No results for {query}</h2>
+                    <ul>Suggestions for improving your results:
+                        <li>Check the spelling or try alternate spellings</li>
+                        <li>Try a more general search, e.g. "pizza" instead of "pepperoni"</li>
+                    </ul>
+
+                </div>
+            )
+        }
+    }
+
     return (
         <>
+            { emptySearch() }
             <ul id="biz-profile-container">
                 {bizItems}
             </ul>
